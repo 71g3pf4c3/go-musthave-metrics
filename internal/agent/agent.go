32dd8cb102197edaa62e4678922027fabdbcbd50
71g3pf4c3 <71g3pf4c3@gmail.com>
2026-03-11 19:47:11 +0300

    Fix bugs

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -112,14 +112,12 @@ func (a *Agent) Run() {
 	reportTicker := time.NewTicker(time.Duration(a.reportInterval) * time.Second)
 	defer pollTicker.Stop()
 	defer reportTicker.Stop()
-	go func() {
-		for {
-			select {
-			case <-pollTicker.C:
-				a.Collect()
-			case <-reportTicker.C:
-				a.Report()
-			}
+	for {
+		select {
+		case <-pollTicker.C:
+			a.Collect()
+		case <-reportTicker.C:
+			a.Report()
 		}
-	}()
+	}
 }
