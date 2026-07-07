e4b7571531f9a71946a9114d2b6ca7cc2a49364b
71g3pf4c3 <71g3pf4c3@gmail.com>
2026-07-07 21:44:13 +0300

    Add build info variables to server and agent for iter23

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/71g3pf4c3/go-musthave-metrics/internal/agent"
@@ -8,7 +9,26 @@ import (
 	"github.com/71g3pf4c3/go-musthave-metrics/internal/logger"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
+func printBuildInfo() {
+	na := func(s string) string {
+		if s == "" {
+			return "N/A"
+		}
+		return s
+	}
+	fmt.Printf("Build version: %s\n", na(buildVersion))
+	fmt.Printf("Build date: %s\n", na(buildDate))
+	fmt.Printf("Build commit: %s\n", na(buildCommit))
+}
+
 func main() {
+	printBuildInfo()
 	cfg, err := config.NewAgentConfig()
 	if err != nil {
 		log.Fatal(err)
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/71g3pf4c3/go-musthave-metrics/internal/config"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
+func printBuildInfo() {
+	na := func(s string) string {
+		if s == "" {
+			return "N/A"
+		}
+		return s
+	}
+	fmt.Printf("Build version: %s\n", na(buildVersion))
+	fmt.Printf("Build date: %s\n", na(buildDate))
+	fmt.Printf("Build commit: %s\n", na(buildCommit))
+}
+
 func main() {
+	printBuildInfo()
 	cfg, err := config.NewServerConfig()
 	if err != nil {
 		log.Fatal(err)
