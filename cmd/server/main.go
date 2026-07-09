4af59f5d8a9d3b54074c5a9998613469944444b0
71g3pf4c3 <71g3pf4c3@gmail.com>
2026-07-09 11:49:50 +0300

    Review fix: extract printBuildInfo into internal/version package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,32 +2,14 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"github.com/71g3pf4c3/go-musthave-metrics/internal/config"
+	"github.com/71g3pf4c3/go-musthave-metrics/internal/version"
 )
-
-var (
-	buildVersion string
-	buildDate    string
-	buildCommit  string
-)
-
-func printBuildInfo() {
-	na := func(s string) string {
-		if s == "" {
-			return "N/A"
-		}
-		return s
-	}
-	fmt.Printf("Build version: %s\n", na(buildVersion))
-	fmt.Printf("Build date: %s\n", na(buildDate))
-	fmt.Printf("Build commit: %s\n", na(buildCommit))
-}
 
 func main() {
-	printBuildInfo()
+	version.Print()
 	cfg, err := config.NewServerConfig()
 	if err != nil {
 		log.Fatal(err)
